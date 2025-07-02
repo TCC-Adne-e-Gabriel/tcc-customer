d2a72ca20c02a52fc7c8c9ed0fb64241b955e009
Adne Moretti <morettiadne1@gmail.com>
2025-07-02 00:56:06 -0300

    fix encrypt

--- app/core/encrypt.py
+++ app/core/encrypt.py
@@ -1,10 +1,11 @@
 import bcrypt
 
 def encrypt_data(password): 
-    bytes = password.encode('utf-8')
+    encoded_password = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    return bcrypt.hashpw(bytes, salt)
+    return bcrypt.hashpw(encoded_password, salt).decode('utf-8')
 
 def check_password(hash, input_password): 
-    userBytes = input_password.encode('utf-8')
-    return bcrypt.checkpw(userBytes, hash)
+    user_bytes = input_password.encode('utf-8')
+    hash_bytes = hash.encode('utf-8')
+    return bcrypt.checkpw(user_bytes, hash_bytes)
